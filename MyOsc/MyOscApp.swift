f2ea5ec8e86cffca11777fff82f415900d3468fe
kabikira <kabikirar2@protonmail.com>
2022-12-13 22:52:50 +0900

    Initial Commit

---
+++ MyOsc/MyOscApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOscApp.swift
+//  MyOsc
+//
+//  Created by koala panda on 2022/12/13.
+//
+
+import SwiftUI
+
+@main
+struct MyOscApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
