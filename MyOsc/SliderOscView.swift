888c62fb86f4faa9f2863853070b259a60cdb9b6
kabikira <kabikirar2@protonmail.com>
2022-12-15 17:39:57 +0900

    add SliderOscView

---
+++ MyOsc/SliderOscView.swift
@@ -0,0 +1,20 @@
+//
+//  SliderOscView.swift
+//  MyOsc
+//
+//  Created by koala panda on 2022/12/14.
+//
+
+import SwiftUI
+
+struct SliderOscView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SliderOscView_Previews: PreviewProvider {
+    static var previews: some View {
+        SliderOscView()
+    }
+}
